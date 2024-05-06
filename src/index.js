0447c0b6af56a0a21e0ae2197f6089864185e49f
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-05-06 17:21:44 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  fetchToys()
+  addNewToy()
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -8,8 +10,108 @@ document.addEventListener("DOMContentLoaded", () => {
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
+
     } else {
       toyFormContainer.style.display = "none";
     }
   });
 });
+
+
+
+
+function fetchToys() {
+  fetch("http://localhost:3000/toys") 
+    .then((res) => res.json())
+    .then((res) => {
+      console.log("response2", res);
+      res.forEach((toy) => addToys(toy));
+    });
+}
+
+
+function addToys(toy){
+  let div = document.getElementById("toy-collection")
+  let card = document.createElement("div")
+  div.appendChild(card)
+  card.className = "card"
+
+  let h2 = document.createElement("h2")
+  card.appendChild(h2)
+  let name = toy.name
+  h2.innerText = name
+
+  let image = document.createElement("img")
+  card.appendChild(image)
+  image.className ="toy-avatar"
+  let imgURL = toy.image
+  image.src = toy.image
+
+  
+
+  let p = document.createElement("p")
+  p.innerText =`${toy.likes} likes` 
+  card.append(p)
+
+  let button = document.createElement("button")
+  card.append(button)
+  button.className = "like-btn"
+  button.id = toy.id
+  button.innerText = "Like"
+  button.addEventListener('click',(event) => {
+    console.log("event", event)
+    event.preventDefault()
+    increment(event)}
+    )
+
+ }
+
+ 
+function addNewToy() {
+  const getButton = document.querySelector(".add-toy-form");
+
+  getButton.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = (document.querySelectorAll(".input-text"));
+
+    const postRequest = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: input[0].value,
+        image: input[1].value,
+        likes: 0,
+      }),
+    };
+
+    fetch("http://localhost:3000/toys", postRequest)
+    .then((res) => res.json())
+    .then(res => console.log(res)) 
+    console.log(input[0].value);
+    console.log(input[1].value);
+  });
+}
+ 
+
+function increment(event) {
+  
+  let count = parseInt(event.target.previousElementSibling.innerText) + 1;
+
+
+  fetch(`http://localhost:3000/toys/${event.target.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept" : "application/json"
+    },
+    body: JSON.stringify({
+      likes: count,
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+    
+}
